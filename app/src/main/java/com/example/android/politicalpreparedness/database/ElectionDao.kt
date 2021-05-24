eb0869c805ed5d4195df11c82a71253b4f483f36
Jean-Peer Lorenz <peer.loz@gmx.net>
2021-05-24 06:59:40 +0200

    feat: implement basic local database using room

--- app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -2,21 +2,31 @@ package com.example.android.politicalpreparedness.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
 import com.example.android.politicalpreparedness.network.models.Election
+import kotlinx.coroutines.flow.Flow
+import java.util.*
 
 @Dao
 interface ElectionDao {
 
-    //TODO: Add insert query
+    // insert query
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertElection(election: Election)
 
-    //TODO: Add select all election query
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllElections(elections: List<Election>)
 
-    //TODO: Add select single election query
+    // select all election query
+    // results are ordered to ensure predictable behaviour
+    @Query("SELECT * FROM election_table WHERE electionDay >= :date ORDER BY electionDay ASC")
+    fun getAllElectionsAfterInclusive(date: Date): Flow<List<Election>>
+
+    // select single election query
+    @Query("SELECT * FROM election_table WHERE id = :id")
+    fun getElectionById(id: Int): Flow<Election>
 
     //TODO: Add delete query
-
     //TODO: Add clear query
-
 }

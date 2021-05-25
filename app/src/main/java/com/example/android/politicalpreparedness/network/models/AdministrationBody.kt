cc2c766683b7e7b40e1f5da88aba74db0d7c0dfe
Jean-Peer Lorenz <peer.loz@gmx.net>
2021-05-25 06:16:30 +0200

    feat: add field for physical address to voter info query response
    This allows to look up an alternative address in case the
    correspondence address is not provided.

--- app/src/main/java/com/example/android/politicalpreparedness/network/models/AdministrationBody.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/models/AdministrationBody.kt
@@ -8,5 +8,6 @@ data class AdministrationBody(
     val electionInfoUrl: String? = null,
     val votingLocationFinderUrl: String? = null,
     val ballotInfoUrl: String? = null,
-    val correspondenceAddress: Address? = null
+    val correspondenceAddress: Address? = null,
+    val physicalAddress: Address? = null
 )
